78bd6907788b185cb6ea4994300ba6c5f56ad426
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-03-01 16:29:34 +0100

    création page

---
+++ app/src/main/java/fr/isen/jal/quizy/PrincipalActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.jal.quizy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}

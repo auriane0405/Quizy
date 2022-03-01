fb85bf1ee62dca3d8d332a6809c95b4aee3e41d0
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-03-01 17:34:33 +0100

    création page

--- app/src/main/java/fr/isen/jal/quizy/HomeActivity.kt
+++ app/src/main/java/fr/isen/jal/quizy/HomeActivity.kt
@@ -1,11 +1,23 @@
 package fr.isen.jal.quizy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityHomeBinding
+    lateinit var buttonCommencer: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_home)
+
+        buttonCommencer = findViewById(R.id.buttonCommencer)
+        val IntentCommencer : Intent =  Intent(this,PrincipalActivity::class.java)
+        buttonCommencer.setOnClickListener {
+            startActivity(IntentCommencer)
+        }
     }
 }
+
--- app/src/main/java/fr/isen/jal/quizy/PrincipalActivity.kt
+++ app/src/main/java/fr/isen/jal/quizy/PrincipalActivity.kt
@@ -1,11 +1,36 @@
 package fr.isen.jal.quizy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PrincipalActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityPrincipalBinding
+    lateinit var retour: Button
+    lateinit var buttonCreer: Button
+    lateinit var buttonJouer: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_principal)
+
+        retour = findViewById(R.id.retour)
+        val Intentretour : Intent =  Intent(this,HomeActivity::class.java)
+        retour.setOnClickListener {
+            startActivity(Intentretour)
+        }
+
+        buttonCreer = findViewById(R.id.buttonCreer)
+        val IntentCreer : Intent =  Intent(this,CreationQuizActivity::class.java)
+        buttonCreer.setOnClickListener {
+            startActivity(IntentCreer)
+        }
+
+        buttonJouer = findViewById(R.id.buttonJouer)
+        val IntentJouer : Intent =  Intent(this,JouerActivity::class.java)
+        buttonJouer.setOnClickListener {
+            startActivity(IntentJouer)
+        }
     }
 }

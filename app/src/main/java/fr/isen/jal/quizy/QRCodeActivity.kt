58ac0c8d7e31a272d2a9afcc77f891315a84bcd3
juliette-roure <juliette.roure@isen.yncrea.fr>
2022-03-07 14:57:12 +0100

    ju

--- app/src/main/java/fr/isen/jal/quizy/QRCodeActivity.kt
+++ app/src/main/java/fr/isen/jal/quizy/QRCodeActivity.kt
@@ -1,11 +1,62 @@
 package fr.isen.jal.quizy
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Build.VERSION_CODES.O
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
-class QRCodeActivity : AppCompatActivity() {
+class QRCodeActivity() : AppCompatActivity() {
+
+    private lateinit var ivQRcode: ImageView
+    private lateinit var NomQuest: TextInputLayout
+    private lateinit var generatorQRcode: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qrcode)
+
+
+        ivQRcode = findViewById(R.id.ivQRcode)
+        NomQuest = findViewById(R.id.NomQuest)
+        generatorQRcode = findViewById(R.id.generatorQRcode)
+
+        generatorQRcode.setOnClickListener {
+            val data = NomQuest.editText.toString().trim()
+
+            if (data.isEmpty()) {
+                Toast.makeText(this, "enter some data", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val writer = QRCodeWriter()
+                try {
+                    val bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, 512, 512)
+                    val width = bitMatrix.width
+                    val height = bitMatrix.height
+                    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+                    for (x in 0 until width) {
+                        for (y in 0 until height) {
+                            bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                        }
+                    }
+                    ivQRcode.setImageBitmap(bmp)
+                } catch (e: WriterException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
+
+

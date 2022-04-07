3959eba6a65b549478e6a7bc2912304ad2809d26
auriane0405 <auriane.coffinet@isen.yncrea.fr>
2022-04-07 11:42:30 +0200

    Page résultats

--- app/src/main/java/fr/isen/jal/quizy/ResultatsActivity.kt
+++ app/src/main/java/fr/isen/jal/quizy/ResultatsActivity.kt
@@ -1,11 +1,20 @@
 package fr.isen.jal.quizy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultatsActivity : AppCompatActivity() {
+    lateinit var buttonMenu: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultats)
+
+        buttonMenu = findViewById(R.id.buttonMenu)
+        val IntentMenu: Intent = Intent(this, PrincipalActivity::class.java)
+        buttonMenu.setOnClickListener {
+            startActivity(IntentMenu)
+        }
     }
 }
